9615fc54e3377f57b6dbc3aec33695d3085a7889
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2012-06-24 12:59:22 +0800

    Adding RCDirectionalPanGestureRecognizer, for horizontal and vertical drag-to-add

---
+++ Dimensions/Libraries/GestureRecognizers/RCDirectionalPanGestureRecognizer.h
@@ -0,0 +1,29 @@
+//
+//  RCVerticalPanRecognizer.h
+//  Dimensions
+//
+//  Created by Ruiwen Chua on 6/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+// Reference taken from http://stackoverflow.com/a/7149691
+
+#import <UIKit/UIKit.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+
+typedef enum {
+    RCPanGestureRecognizerVertical,
+    RCPanGestureRecognizerHorizontal
+} RCPanGestureRecognizerDirection;
+
+
+@interface RCDirectionalPanGestureRecognizer : UIPanGestureRecognizer {
+	int _moveX;
+	int _moveY;
+	RCPanGestureRecognizerDirection _direction;
+}
+
+@property (nonatomic, assign) RCPanGestureRecognizerDirection direction;
+
+@end
