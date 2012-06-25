66b9ce9a7b21493c09528d07ee6951b62fcabd2f
Ruiwen Chua <ruiwen+github@thoughtmonkeys.com>
2012-06-26 01:16:34 +0800

    Adding VerticalLine : DMLine to project

---
+++ Dimensions/VerticalLine.h
@@ -0,0 +1,16 @@
+//
+//  VerticalLine.h
+//  Dimensions
+//
+//  Created by Ruiwen Chua on 6/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DMLine.h"
+
+@interface VerticalLine : DMLine
+@property (strong, nonatomic) IBOutlet UIView *line;
+@property (strong, nonatomic) IBOutlet UILabel *label;
+
+@end
---
+++ Dimensions/VerticalLine.m
@@ -0,0 +1,33 @@
+//
+//  VerticalLine.m
+//  Dimensions
+//
+//  Created by Ruiwen Chua on 6/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "VerticalLine.h"
+
+@implementation VerticalLine
+@synthesize line;
+@synthesize label;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
